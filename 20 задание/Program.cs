6fac666682649a15dbed8e02071a28adc786bed8
pavel7799 <81684192+pavel7799@users.noreply.github.com>
2021-05-26 14:58:53 +0300

    Add files via upload

---
+++ 20 задание/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string b,a,c;
+            Console.WriteLine("Введите строку");
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
