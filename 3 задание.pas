846bca20791683d7c0fe1666cfddb9fce0c575d9
pavel7799 <81684192+pavel7799@users.noreply.github.com>
2021-05-26 14:47:41 +0300

    Add files via upload

---
+++ 3 задание.pas
@@ -0,0 +1,11 @@
+﻿var a:string;
+begin
+writeln('Как тебя зовут ?');
+readln(a);
+writeln('выдан разделом УФМС ');
+readln();
+writeln('год рождения?');
+readln();
+writeln('страна проживания');
+readln();
+end.
