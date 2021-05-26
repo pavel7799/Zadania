1acaf82029ae40fa8f5b4c01055e757e3e0f8736
pavel7799 <81684192+pavel7799@users.noreply.github.com>
2021-05-26 15:17:28 +0300

    Add files via upload

---
+++ 24 задание.pas
@@ -0,0 +1,13 @@
+﻿program aasdasd;
+uses crt;
+var s:string;
+    n,i:integer;
+begin
+writeln ('Введите строку;');
+read(s);
+n:=length(s);
+for i:=1 to n do
+if s[i] in ['a'..'z','а'..'я'] then s[i]:=chr(ord(s[i])-32)
+else if s[i]='ё' then s[i]:='Ё';
+write(s);
+end.
