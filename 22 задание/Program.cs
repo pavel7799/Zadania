8461dd19ea0a297ba11c4a4f4d80d0d8f4ab3561
pavel7799 <81684192+pavel7799@users.noreply.github.com>
2021-05-26 14:59:48 +0300

    Add files via upload

---
+++ 22 задание/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите десятичное число, цифры нечетных позиций у которого будут посчитаны");
+            long N = long.Parse(Console.ReadLine());
+            long s = 0;
+            while (N > 0)
+            {
+                if (N % 2 > 0)
+                {
+                    s = s + N % 10;
+                }
+                N = N / 10;
+            }
+            Console.WriteLine("Сумма нечетных цифр числа = " + s);
+            Console.ReadKey();
+        }
+    }
+}
