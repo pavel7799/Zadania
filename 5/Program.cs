ad4cc4853077820df8b673017868dbdc7bdb3c6b
pavel7799 <81684192+pavel7799@users.noreply.github.com>
2021-05-26 14:01:55 +0300

    Add files via upload

---
+++ 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a;
+            double b = 0, c; 
+            for (a = 1; a <= 20; a++)
+            {
+                c = b;
+                b = 1 / Math.Pow(2, a);
+                b = Math.Round(c + b,7);
+            }
+            
+            Console.WriteLine($"S="+ b.ToString());
+            
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+    }
+}
