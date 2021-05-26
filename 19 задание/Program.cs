3fc982e358f8017a391b164a4e57674fb567ed5c
pavel7799 <81684192+pavel7799@users.noreply.github.com>
2021-05-27 00:52:04 +0300

    Add files via upload

---
+++ 19 задание/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Random r = new Random();
+            for (int j = 0; j < n; j++)
+            {
+                arr[j] = r.Next(1, 50);
+                Console.Write(arr[j] + " ");
+            }
+            Console.WriteLine();
+            int x = Convert.ToInt32(Console.ReadLine());
+            int[] arr2 = new int[n];
+            Random r2 = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                arr2[i] = r2.Next(-x, x) * x;
+                Console.Write(arr2[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
